eb9331b6467edc1fee5aa4561637172db789b5d6
firstarachne0116 <firstarachne0116@gmail.com>
2015-12-04 06:29:50 +0800

    Merge branch 'feature/jinja'

--- setup.py
+++ setup.py
@@ -31,10 +31,11 @@ install_requires = [
     "TurboGears2",
     "tg.devtools",
     "WebOb==1.1.1",
+    "WebTest < 2.0"
     "Genshi",
     "zope.sqlalchemy >= 0.4",
     "repoze.tm2 >= 1.0a5",
-    "sqlalchemy == 0.7.9",
+    "sqlalchemy < 0.8",
     "repoze.what >= 1.0.8",
     "repoze.who-friendlyform >= 1.0.4",
     "repoze.what-pylons >= 1.0",
