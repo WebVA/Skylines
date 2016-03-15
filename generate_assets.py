8d75e436bf210cf355754316e1a24d3e578e637c
firstarachne0116 <firstarachne0116@gmail.com>
2016-03-16 02:48:40 +0800

    Merge pull request #38 from Turbo87/config-defaults

--- generate_assets.py
+++ generate_assets.py
@@ -4,8 +4,11 @@ import os
 import sys
 import gzip
 from argparse import ArgumentParser
-from skylines import assets
-from skylines.config import environment
+from paste.deploy.loadwsgi import appconfig
+from skylines.assets import Environment
+
+PRO_CONF_PATH = '/etc/skylines/production.ini'
+DEV_CONF_PATH = 'development.ini'
 
 # Build paths
 base_path = os.path.dirname(sys.argv[0])
@@ -20,13 +23,19 @@ parser.add_argument('bundles_module', nargs='?', metavar='skylines.assets.bundle
 # Parse arguments
 args = parser.parse_args()
 
+if args.conf_path is not None:
+    if not os.path.exists(args.conf_path):
+        parser.error('Config file "{}" not found.'.format(args.conf_path))
+elif os.path.exists(PRO_CONF_PATH):
+    args.conf_path = PRO_CONF_PATH
+else:
+    args.conf_path = DEV_CONF_PATH
+
 # Load config from file
-conf = environment.load_from_file(args.conf_path)
-if not conf:
-    parser.error('Config file "{}" not found.'.format(args.conf_path))
+conf = appconfig('config:' + os.path.abspath(args.conf_path))
 
 # Create assets environment
-env = assets.Environment(conf)
+env = Environment(conf)
 
 # Load the bundles from the YAML file
 if args.bundles_module is None:
