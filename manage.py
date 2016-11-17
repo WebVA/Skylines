622863b563d414f61524d9766fc2175b8f1c4a82
firstarachne0116 <firstarachne0116@gmail.com>
2016-11-17 18:56:29 +0800

    scripts: Converted import_srtm.py to manage.py command

--- manage.py
+++ manage.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python
 
 import sys
-from flask.ext.script import Manager
-from skylines import create_combined_app
+
+from flask import current_app
+from flask.ext.script import Manager, Shell
+
+from skylines.app import create_combined_app
+from skylines import model
+from skylines.commands import ImportSRTM
 from config import to_envvar
 
 
@@ -13,8 +18,15 @@ def create_app(config):
 
     return create_combined_app()
 
+
+def _make_context():
+    return dict(app=current_app, model=model, db=model.db)
+
+
 manager = Manager(create_app)
 manager.add_option('-c', '--config', dest='config', required=False)
+manager.add_command("shell", Shell(make_context=_make_context))
+manager.add_command("import-srtm", ImportSRTM())
 
 if __name__ == "__main__":
     manager.run()
