426f278c37b17c1ae1864aa36ec9efc5bb5aca15
firstarachne0116 <firstarachne0116@gmail.com>
2017-01-11 00:22:21 +0800

    INSTALL.md: fix note about postgis 2.1.3

--- skylines/frontend/views/upload.py
+++ skylines/frontend/views/upload.py
@@ -8,16 +8,14 @@ from flask.ext.babel import _, lazy_gettext as l_
 from redis.exceptions import ConnectionError
 from werkzeug.exceptions import BadRequest
 
-from skylines.frontend.forms import UploadForm, UploadUpdateForm
+from skylines.frontend.forms import UploadForm, ChangeAircraftForm
 from skylines.lib import files
 from skylines.lib.decorators import login_required
 from skylines.lib.md5 import file_md5
-from skylines.lib.xcsoar_ import flight_path, analyse_flight
+from skylines.lib.xcsoar_ import analyse_flight
 from skylines.model import db, User, Flight, IGCFile
 from skylines.model.event import create_flight_notifications
 from skylines.worker import tasks
-
-import xcsoar
 
 upload_blueprint = Blueprint('upload', 'skylines')
 
@@ -70,19 +68,6 @@ def IterateUploadFiles(upload):
             yield x
 
 
-def _get_flight_path(flight):
-    fp = flight_path(flight.igc_file, add_elevation=True)
-
-    barogram_h = xcsoar.encode([fix.altitude for fix in fp], method="signed")
-    barogram_t = xcsoar.encode([fix.seconds_of_day for fix in fp], method="signed")
-    enl = xcsoar.encode([fix.enl if fix.enl is not None else 0 for fix in fp], method="signed")
-    elevations_h = xcsoar.encode([fix.elevation if fix.elevation is not None else -1000 for fix in fp], method="signed")
-
-    return dict(barogram_h=barogram_h, barogram_t=barogram_t,
-                enl=enl, elevations_h=elevations_h,
-                igc_start_time=fp[0].datetime, igc_end_time=fp[-1].datetime)
-
-
 @upload_blueprint.route('/', methods=('GET', 'POST'))
 @login_required(l_("You have to login to upload flights."))
 def index():
@@ -103,19 +88,15 @@ def index():
             except ValueError:
                 raise BadRequest('Status unknown')
 
-            flight, trace, form = check_update_form(prefix, flight_id, name, status)
+            flight, form = check_update_form(prefix, flight_id, name, status)
 
-            flights.append((name, flight, status, str(prefix), trace, form))
+            flights.append((name, flight, status, str(prefix), form))
 
             if form and form.validate_on_submit():
                 _update_flight(flight_id,
                                form.model_id.data,
                                form.registration.data,
-                               form.competition_id.data,
-                               form.takeoff_time.data,
-                               form.scoring_start_time.data,
-                               form.scoring_end_time.data,
-                               form.landing_time.data)
+                               form.competition_id.data)
                 flight_id_list.append(flight_id)
             elif form:
                 form_error = True
@@ -163,7 +144,7 @@ def index_post(form):
             other = Flight.by_md5(md5)
             if other:
                 files.delete_file(filename)
-                flights.append((name, other, UploadStatus.DUPLICATE, str(prefix), None, None))
+                flights.append((name, other, UploadStatus.DUPLICATE, str(prefix), None))
                 continue
 
         igc_file = IGCFile()
@@ -174,7 +155,7 @@ def index_post(form):
 
         if igc_file.date_utc is None:
             files.delete_file(filename)
-            flights.append((name, None, UploadStatus.MISSING_DATE, str(prefix), None, None))
+            flights.append((name, None, UploadStatus.MISSING_DATE, str(prefix), None))
             continue
 
         flight = Flight()
@@ -194,25 +175,21 @@ def index_post(form):
 
         if not analyse_flight(flight):
             files.delete_file(filename)
-            flights.append((name, None, UploadStatus.PARSER_ERROR, str(prefix), None, None))
+            flights.append((name, None, UploadStatus.PARSER_ERROR, str(prefix), None))
             continue
 
         if not flight.takeoff_time or not flight.landing_time:
             files.delete_file(filename)
-            flights.append((name, None, UploadStatus.NO_FLIGHT, str(prefix), None, None))
+            flights.append((name, None, UploadStatus.NO_FLIGHT, str(prefix), None))
             continue
 
         if not flight.update_flight_path():
             files.delete_file(filename)
-            flights.append((name, None, UploadStatus.NO_FLIGHT, str(prefix), None, None))
+            flights.append((name, None, UploadStatus.NO_FLIGHT, str(prefix), None))
             continue
 
-        flight.privacy_level = Flight.PrivacyLevel.PRIVATE
-
-        trace = _get_flight_path(flight)
-        flights.append((name, flight, UploadStatus.SUCCESS, str(prefix), trace,
-                        UploadUpdateForm(formdata=None, prefix=str(prefix), obj=flight)))
-
+        flights.append((name, flight, UploadStatus.SUCCESS, str(prefix),
+                        ChangeAircraftForm(formdata=None, prefix=str(prefix), obj=flight)))
         db.session.add(igc_file)
         db.session.add(flight)
 
@@ -225,13 +202,21 @@ def index_post(form):
 
     db.session.commit()
 
+    try:
+        for flight in flights:
+            if flight[2] is UploadStatus.SUCCESS:
+                tasks.analyse_flight.delay(flight[1].id)
+                tasks.find_meetings.delay(flight[1].id)
+    except ConnectionError:
+        current_app.logger.info('Cannot connect to Redis server')
+
     return render_template(
         'upload/result.jinja', num_flights=prefix, flights=flights, success=success)
 
 
 def check_update_form(prefix, flight_id, name, status):
     if not flight_id:
-        return None, None, None
+        return None, None
 
     # Get flight from database and check if it is writable
     flight = Flight.get(flight_id)
@@ -240,28 +225,18 @@ def check_update_form(prefix, flight_id, name, status):
         abort(404)
 
     if status == UploadStatus.DUPLICATE:
-        return flight, None, None
+        return flight, None
 
     else:
         if not flight.is_writable(g.current_user):
             abort(403)
 
-        form = UploadUpdateForm(prefix=str(prefix), obj=flight)
-        trace = _get_flight_path(flight)
+        form = ChangeAircraftForm(prefix=str(prefix), obj=flight)
 
-        # Force takeoff_time and landing_time to be within the igc file limits
-        if form.takeoff_time.data < trace['igc_start_time']:
-            form.takeoff_time.data = trace['igc_start_time']
-
-        if form.landing_time.data > trace['igc_end_time']:
-            form.landing_time.data = trace['igc_end_time']
-
-        return flight, trace, form
+        return flight, form
 
 
-def _update_flight(flight_id, model_id, registration, competition_id,
-                   takeoff_time, scoring_start_time,
-                   scoring_end_time, landing_time):
+def _update_flight(flight_id, model_id, registration, competition_id):
     # Get flight from database and check if it is writable
     flight = Flight.get(flight_id)
 
@@ -288,34 +263,6 @@ def _update_flight(flight_id, model_id, registration, competition_id,
     flight.competition_id = competition_id
     flight.time_modified = datetime.utcnow()
 
-    # Update times only if they are reasonable and have been changed...
-    trigger_analysis = False
-
-    if takeoff_time and scoring_start_time and scoring_end_time and landing_time \
-       and takeoff_time <= scoring_start_time <= scoring_end_time <= landing_time \
-       and (flight.takeoff_time != takeoff_time
-            or flight.scoring_start_time != scoring_start_time
-            or flight.scoring_end_time != scoring_end_time
-            or flight.landing_time != landing_time):
-
-        flight.takeoff_time = takeoff_time
-        flight.scoring_start_time = scoring_start_time
-        flight.scoring_end_time = scoring_end_time
-        flight.landing_time = landing_time
-
-        trigger_analysis = True
-
-    flight.privacy_level = Flight.PrivacyLevel.PUBLIC
-
     db.session.commit()
-
-    if trigger_analysis:
-        analyse_flight(flight)
-
-    try:
-        tasks.analyse_flight.delay(flight.id)
-        tasks.find_meetings.delay(flight.id)
-    except ConnectionError:
-        current_app.logger.info('Cannot connect to Redis server')
 
     return True
