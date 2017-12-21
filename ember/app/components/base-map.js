6b0c78ab697bb309aec7f9837da55690fb77a91d
firstarachne0116 <firstarachne0116@gmail.com>
2017-12-21 12:19:23 +0800

    Merge pull request #504 from Turbo87/cleanup

--- ember/app/components/base-map.js
+++ ember/app/components/base-map.js
@@ -66,7 +66,6 @@ export default Ember.Component.extend({
     this.addMWPLayers();
     this.addBingLayers();
     this.addMapboxLayer();
-    this.addEmptyLayer();
 
     let query = parseQueryString(window.location.search);
     this.set('baseLayer', query.baselayer);
@@ -266,18 +265,4 @@ export default Ember.Component.extend({
     map.addLayer(road);
     map.addLayer(hybrid);
   },
-
-  addEmptyLayer() {
-    let empty_layer = new ol.layer.Tile({
-    });
-    empty_layer.setProperties({
-      'name': 'Empty',
-      'id': 'Empty',
-      'base_layer': true,
-      'display_in_layer_switcher': true,
-    });
-
-    this.get('map').addLayer(empty_layer);
-  },
-
 });
