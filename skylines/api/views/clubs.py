f5ae10e321bc752708e8e4dae9ee2f0c63a454fe
firstarachne0116 <firstarachne0116@gmail.com>
2018-06-14 16:07:53 +0800

    Bump ember-concurrency from 0.8.18 to 0.8.19 in /ember

--- skylines/api/views/clubs.py
+++ skylines/api/views/clubs.py
@@ -6,7 +6,7 @@ from skylines.database import db
 from skylines.api.oauth import oauth
 from skylines.lib.dbutil import get_requested_record
 from skylines.model import Club, User
-from skylines.model.notification import (
+from skylines.model.event import (
     create_club_join_event
 )
 
