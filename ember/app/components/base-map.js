a618a7b7d2ababafd09bb2f46f787aa2021c91ad
firstarachne0116 <firstarachne0116@gmail.com>
2018-11-16 03:28:33 +0800

    Caddy: Simplify  configuration

--- ember/app/components/base-map.js
+++ ember/app/components/base-map.js
@@ -109,6 +109,8 @@ export default Component.extend({
   },
 
   addMWPLayers() {
+    let tile_url = this.getWithDefault('mapTileURL', '');
+
     let mwp_layer = new ol.layer.Tile({
       source: new ol.source.XYZ({
         attributions: [
@@ -120,7 +122,7 @@ export default Component.extend({
               '</a>.',
           }),
         ],
-        url: `${config.SKYLINES_TILE_BASEURL || ''}/tiles/1.0.0/mwp/EPSG3857/{z}/{x}/{y}.png`,
+        url: `${tile_url}/tiles/1.0.0/mwp/{z}/{x}/{y}.png`,
       }),
       zIndex: 11,
     });
@@ -138,7 +140,7 @@ export default Component.extend({
   addAirspaceLayers() {
     let airspace_layer = new ol.layer.Tile({
       source: new ol.source.XYZ({
-        url: `${config.SKYLINES_TILE_BASEURL || ''}/tiles/1.0.0/airspace+airports/EPSG3857/{z}/{x}/{y}.png`,
+        url: `${config.SKYLINES_TILE_BASEURL || ''}/tiles/1.0.0/airspace+airports/{z}/{x}/{y}.png`,
       }),
       zIndex: 10,
     });
