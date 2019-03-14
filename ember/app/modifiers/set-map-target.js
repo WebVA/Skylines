de74241fa9c459eb6cc5f57ed40638d16638ab30
firstarachne0116 <firstarachne0116@gmail.com>
2019-03-14 12:23:45 +0800

    BsButton: Fix  deprecation warning

--- ember/app/modifiers/set-map-target.js
+++ ember/app/modifiers/set-map-target.js
@@ -1,6 +1,6 @@
-import { modifier } from 'ember-modifier';
+import makeFunctionalModifier from 'ember-functional-modifiers';
 
-export default modifier((element, [map]) => {
+export default makeFunctionalModifier((element, [map]) => {
   if (map) {
     map.setTarget(element);
     return () => map.setTarget(null);
