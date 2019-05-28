8e97a50161f8da8c9d7120df55def99bd70e02b8
firstarachne0116 <firstarachne0116@gmail.com>
2019-05-28 16:06:38 +0800

    CesiumPlaneModel: Convert to Glimmer component

--- ember/app/components/cesium-plane-model.js
+++ ember/app/components/cesium-plane-model.js
@@ -1,17 +1,11 @@
 /* globals Cesium */
 
-import Component from '@ember/component';
+import { action } from '@ember/object';
+import Component from '@glimmer/component';
 
 import { transform } from 'ol/proj';
 
-import safeComputed from '../computed/safe-computed';
-
 export default class CesiumPlaneModel extends Component {
-  tagName = '';
-
-  scene = null;
-  fix = null;
-
   entity = Cesium.Model.fromGltf({
     url: '../../3d/AS21.glb',
     scale: 1,
@@ -19,27 +13,23 @@ export default class CesiumPlaneModel extends Component {
     allowPicking: false,
   });
 
-  @safeComputed('coordinate', coordinate => {
+  @action
+  update([coordinate, heading]) {
     let lonlat = transform(coordinate, 'EPSG:3857', 'EPSG:4326');
-    return Cesium.Cartesian3.fromDegrees(lonlat[0], lonlat[1], lonlat[2]);
-  })
-  position;
 
-  didReceiveAttrs() {
-    super.didReceiveAttrs(...arguments);
-    this.entity.modelMatrix = Cesium.Transforms.headingPitchRollToFixedFrame(
-      this.position,
-      new Cesium.HeadingPitchRoll(this.heading, 0, 0),
-    );
+    let position = Cesium.Cartesian3.fromDegrees(lonlat[0], lonlat[1], lonlat[2]);
+    let rotation = new Cesium.HeadingPitchRoll(heading, 0, 0);
+
+    this.entity.modelMatrix = Cesium.Transforms.headingPitchRollToFixedFrame(position, rotation);
   }
 
-  didInsertElement() {
-    super.didInsertElement(...arguments);
-    this.scene.primitives.add(this.entity);
+  constructor() {
+    super(...arguments);
+    this.args.scene.primitives.add(this.entity);
   }
 
-  willDestroyElement() {
-    super.willDestroyElement(...arguments);
-    this.scene.primitives.remove(this.entity);
+  willDestroy() {
+    super.willDestroy(...arguments);
+    this.args.scene.primitives.remove(this.entity);
   }
 }
